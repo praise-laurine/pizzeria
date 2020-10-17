0893f3c7cd1f530273a37945997577d5440a01c1
praise-laurine <praiselaurine10@gmail.com>
2020-10-17 22:18:33 +0300

    added a function

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    function Pizzeria(size,topping,crust,flavor){
+        this.flavor =flavor;
+        this.crust = crust;
+        this.topping = topping;
+        this.size = size; 
+    }
+
+})
