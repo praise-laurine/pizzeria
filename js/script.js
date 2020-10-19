61c7c2f8d5a75d0545041da3b97eeb122774deaa
praise-laurine <praiselaurine10@gmail.com>
2020-10-19 05:55:25 +0300

    added multiple functions

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         this.topping = topping;
         this.size = size;
     }
-    // var totalAmount = 0;
+    var totalAmount = 0;
     var OrderNew = [];
     Pizzeria.prototype.ToppingPrice = function () {
         if (this.size === "large") {
@@ -114,8 +114,8 @@ $(document).ready(function () {
             }
         }
     }
-    Pizzeria.prototype.pizzaPrice = function () {
-        return this.CrustPrice() + this.ToppingPrice() + this.flavorPrice()
+    Pizzeria.prototype.flavorPrice = function () {
+        return this.crustPrice() + this.ToppingPrice() 
     }
 
     $("#order-form").submit(function (event) {
@@ -132,8 +132,105 @@ $(document).ready(function () {
         $("#crust").val("");
         $("#toppings").val("");
 
+        totalAmount = 0
+        for (let i = 0; i < OrderNew.length; i++ ){
+            totalAmount += OrderNew[i].flavorPrice();
+        }
+        $("#ordersTaken").append(
+            "<tr>" +
+            '<th scope="orderCalculation">' +
+            newPizza.flavor +
+            " - " +
+            newPizza.size +
+            "  @ " +
+            newPizza.flavorPrice() +
+            "</th>" +
+            "<td>" +
+            newPizza.topping +
+            " @ " +
+            newPizza.ToppingPrice() +
+            "</td>" +
+            "<td>" +
+            newPizza.crust +
+            " @ " +
+            newPizza.crustPrice() +
+            "</td>" +
+            "<td>" +
+            newPizza.flavorPrice() +
+            "</td>" +
+            "</tr>"
+        );
 
-    });
+        $("#order-summary").append("");
+        if (OrderNew.length > 0) {
+            $("#form-heading").empty();
+            $("#form-heading").append("Make A New Order");
+        }
+        $("#totalAmount").fadeIn();
+        $("#Checkout").fadeIn();
+        $("#orderClass").fadeIn();
+
+        $("#totalAmount").empty();
+        $("#totalAmount").append(totalAmount);
+        $(".totalAmount").show();
+
+});
+
+$("#Checkout").click(function () {
+    $(".checkout-info").show();
+});
+
+$("#checkoutForm").submit(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var name = $("#phone").val();
+    var deliveryOption = $("#OptionDelivery").val();
+    NameOfCustomer = name;
+    $("#name").val("");
+    $("#OptionDelivery").val("");
+    $(".checkout-info").hide();
+    $("#Checkout").hide();
+    $("#totalAmount").empty();
+    if (deliveryOption === "Home Delivery?") {
+        $(".deliveryLocation").show();
+        $(".deliveryCost").show();
+        $("#totalAmount").empty();
+        $("#delivery-cost").append(400);
+        totalAmount += 400;
+        $("#totalAmount").empty();
+        $("#totalAmount").empty();
+    $("#totalAmount").append(totalAmount);
+    $(".TotalAmount").show();
+
+        $("#totalAmount").append(totalAmount);
+    } else {
+        alert(NameOfCustomer + ": Your total bill is Ksh. " + totalAmount + ". Your order will be ready for collection in the next one hours." + " Make sure you have the total amount for the order during the delivery to avoid delays." + " Incase there is any comment of complaint, Please reach out to us as soon as possible.");
+    }
+});
+
+$("#locationForm").submit(function (event) {
+    event.preventDefault();
+    var estateEntered = $("#estate").val();
+    var houseNumberEntered = $("#houseNumber").val();
+    estate = estateEntered;
+    houseNumber = houseNumberEntered;
+    $("#Checkout").hide();
+    $(".deliveryLocation").hide();
+    $("#totalAmount").empty();
+    $("#totalAmount").append(totalAmount);
+    $(".TotalAmount").show();
+    alert(NameOfCustomer + ": Your new total bill is Ksh. " + totalAmount + ". Your order will be delivered to " + estate + ", " + houseNumber + " in the next one hour." + " Make sure you have the total amount for the order during the delivery to avoid delays." + " Incase there is any comment of complaint, Please reach out to us as soon as possible");
+});
+// $("#contacts").submit(function (event) {
+//     event.preventDefault();
+//     var name1 = $("#form1").val();
+//     var email1 = $("#form2").val();
+//     var message1 = $("#form3").val();
+//     alert("Hello! " + name1 + "," + " We have received your message. Thank you for reaching out to us. ");
+
+// });
+
+
 
 
 });
